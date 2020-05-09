6a7859fa35d4f40a3359cd6bf34f164a36e84934
congiu.gaetan <gaetan.congiu@esqese.ucly.fr>
2020-05-09 17:28:52 +0200

    Ajout des champs mdp et identifiant

--- login.php
+++ login.php
@@ -9,5 +9,30 @@
 </head>
 <link rel="stylesheet" href="./style/login.css">
 <body>
+    <div class="accueil">
+        <div class="donnée utilisateur">
+            Entrer votre pseudo et votre mot de passe
+            <form action ="login.php" method="post">
+                <table>
+                    <tr>
+                        <td>
+                            <label for="identifiant">Identifiant</label> :
+                        </td>
+                        <td>
+                            <input type="text" class="barre" name="identifiant" id="identifiant"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="mdp">Mot de passe</label> :
+                        </td>
+                        <td>
+                            <input type="password" class="barre" name="mdp" id="mdp" required/>
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+    </div>
 </body>
 
