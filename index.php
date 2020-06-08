184ae42536fdcf72c09e4330fb8b2354ef45e1f2
clement.theotime <theotime.clement@esqese.ucly.fr>
2020-06-08 11:21:50 +0200

    gaetan

--- index.php
+++ index.php
@@ -1,56 +1,47 @@
 <?php
+$jsondata = json_decode(file_get_contents("film.json"));
 ?>
+
+
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
 <head>
     <title> JUSTCHILL</title> <!-- titre de l'onglet -->
     <meta charset = "utf-8" />
-    <script src = "stream.js"> </script>
     <link rel = "stylesheet" type = "text/css" href = "stream.css" />
-    <link href="https://fonts.googleapis.com/css2?family=Bitter:wght@700&display=swap" rel="stylesheet">/*lien google pour avoir la police*/
+    <link href="https://fonts.googleapis.com/css2?family=Bitter:wght@700&display=swap" rel="stylesheet">
 </head>
-
 <body>
-/*le haut de la page avec le logo*/
+
+
 <div class="head"> <img src="image/logo.png" alt="logo" id="logo"> </div><br><br>
 <div id="miseformeaffiche">
-    /*la categorie nouveau*/
     <div id="categorie"><h2> Nouveautées</h2></div>
-
-    <div id="encadrementFilm">
-        <img src="image/bigbunny.PNG" alt="bigbunny" alt="bigbunnymovie" id="image"
-             onmouseover = "hoverPlay(this)" onmouseout = "unhoverbunny(this)" onclick="lecteurpage(1)">
-        <div id="titrefilm">
-            <h2 id="styletitrefilm"> Big Buck Bunny </h2>
+    <?php foreach($jsondata as $film): ?>
+        <div id="encadrementFilm">
+            <img src="image/<?php echo $film->photoFilm ?>" alt="<?php echo $film->titre ?>" id="image"
+                 onmouseover = "hoverPlay(this)" onmouseout = "unhover<?php echo $film->titrecourt ?>
+                    (this)" onclick="lecteurpage<?php echo $film->numerodefilm?>()">
+            <div id="titrefilm">
+                <h2 id="styletitrefilm"> <?php echo $film->titre ?> </h2>
+            </div>
         </div>
-    </div>
+        <script type='text/javascript'>
+            function hoverPlay(element) {
+                element.setAttribute('src', 'image/play.PNG');
+            }
+            function unhover<?php echo $film->titrecourt ?>(element) {
+                element.setAttribute('src', 'image/<?php echo $film->photoFilm ?>');
+            }
+            function lecteurpage<?php echo $film->numerodefilm?>() {
 
-    <div id="encadrementFilm">
-        <img src="image/john.PNG" alt="JohnHenry"  id="image"
-             onmouseover = "hoverPlay(this)" onmouseout = "unhoverjohn(this)" onclick="lecteurpage(2)">
-        <div id="titrefilm">
-            <h2 id="styletitrefilm"> John Henry </h2>
-        </div>
-    </div>
+                window.location = "<?php echo $film->film ?>";
 
-    <div id="encadrementFilm">
-        <img src="image/fast.PNG" alt="fastfurious"  id="image"
-             onmouseover = "hoverPlay(this)" onmouseout = "unhoverfast(this)" onclick="lecteurpage(3)">
-        <div id="titrefilm">
-            <h2 id="styletitrefilm"> Fast And Furious</h2>
-        </div>
-    </div>
+            }
+        </script>
 
-    <div id="encadrementFilm">
-        <img src="image/extration.PNG" alt="Extration"  id="image"
-             onmouseover = "hoverPlay(this)" onmouseout = "unhoverextra(this)" onclick="lecteurpage(4)">
-        <div id="titrefilm">
-            <h2 id="styletitrefilm"> Extration </h2>
-        </div>
-    </div>
-
+    <?php endforeach;?>
 </div>
-
 
 
 </body>
